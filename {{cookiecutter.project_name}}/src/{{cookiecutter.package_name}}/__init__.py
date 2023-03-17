0930fca649953be94c2e9f7ac7518083f5670d52
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2023-03-18 00:21:30 +0530

    Remove duplicate cli function

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -5,8 +5,3 @@ def some_function() -> str:
     """Returns a string."""
 
     return "This is a string"
-
-
-def cli() -> None:
-    """CLI interface."""
-    print("Hello, this is a sample CLI output.")
